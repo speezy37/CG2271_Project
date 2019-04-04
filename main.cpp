29a8f157ff27ccb469a1f33f0ff0635078da2f26
pinjuen <limpinjuen@gmail.com>
2019-04-04 19:01:43 +0800

    code updated

--- main.cpp
+++ main.cpp
@@ -7,12 +7,17 @@
 #include "main.h"
 
 void setup() {
-	xTaskCreate(TaskRedLED, "LED", STACK_SIZE, NULL, 1, NULL);
-	vTaskStartScheduler();
+    Serial.begin(9600);
+    pinMode(PIN_AUDIO, OUTPUT);
 }
 
 void loop() {
-
+    xTaskCreate(TaskRedLED, "redLED", STACK_SIZE, NULL, 2, NULL);
+//  xTaskCreate(TaskGreenLED(), "greenLED", STACK_SIZE, NULL, 2, NULL);
+	xTaskCreate(TaskAudio, "Audio", STACK_SIZE, NULL, 2, NULL);
+	xTaskCreate(TaskSerial, "Serial", STACK_SIZE, NULL, 2, NULL);
+	xTaskCreate(TaskMotor, "Motor", STACK_SIZE, NULL, 2, NULL);
+	vTaskStartScheduler();
 }
 
 void TaskRedLED(void *p)  // This is a task.
@@ -23,6 +28,17 @@ void TaskRedLED(void *p)  // This is a task.
 
   while(1)
   {
+    if (blueToothVal == BT_CONNECTED){	//BT connection established
+        digitalWrite(PIN_REDLED, HIGH);
+        vTaskDelay(500);
+        digitalWrite(PIN_REDLED, LOW);
+        vTaskDelay(500);
+        digitalWrite(PIN_REDLED, HIGH);
+        vTaskDelay(500);
+        digitalWrite(PIN_REDLED, LOW);
+        vTaskDelay(500);
+        blueToothVal = EMPTY;
+    }
 
 	if(moveState == Stationary) {
 		period = 250;
@@ -42,7 +58,9 @@ void TaskSerial(void *p)
 {
 	while(1)
 	{
-
+        if (Serial.available()){	//if there is data being received
+			blueToothVal = Serial.read();
+		}
 	}
 }
 
@@ -56,9 +74,42 @@ void TaskMotor(void *p)
 
 void TaskAudio(void *p)
 {
-	while(1)
-	{
+    int specialTuneBT[3] = {261, 294, 330};
 
+	int specialTuneEnd[3] = {330, 294, 261};
+
+	int babyShark[30] = {
+	    294, 330, 392, 392, 392, 392, 392, 392, 392,
+		294, 330, 392, 392, 392, 392, 392, 392, 392,
+		294, 330, 392, 392, 392, 392, 392, 392, 392,
+		392, 392, 370};
+
+	while(1) {
+		if (blueToothVal == BT_CONNECTED){
+			for(int i = 0; i < 3; i++){
+				tone(PIN_AUDIO, specialTuneBT[i]);
+				vTaskDelay(300);
+				noTone(PIN_AUDIO);
+				vTaskDelay(100);
+				blueToothVal = EMPTY;
+			}
+		}
+		else if (blueToothVal == BABY_SHARK){
+			while (blueToothVal != OFF){
+				for(int i = 0; i < 30; i++){
+					tone(PIN_AUDIO, babyShark[i]);
+					vTaskDelay(300);
+					noTone(PIN_AUDIO);
+					vTaskDelay(100);
+				}
+			}
+			for(int i = 0; i < 3; i++){
+				tone(PIN_AUDIO, specialTuneEnd[i]);
+				vTaskDelay(300);
+				noTone(PIN_AUDIO);
+				vTaskDelay(100);
+			}
+		}
 	}
 }
 
--- main.h
+++ main.h
@@ -33,17 +33,29 @@
 #define PIN_MOTORBL	6
 
 // Buzzer
-#define PIN_AUDIO	3
+#define PIN_AUDIO   3
 
 // Serial
 #define PIN_RX	0
 #define PIN_TX	1
 
+// Bluetooth
+#define BT_CONNECTED 'c'
+#define OFF 'f'
+#define BABY_SHARK 'b'
+#define EMPTY 'e'
+#define LEFT 'L'
+#define RIGHT 'R'
+#define FORWARD 'F'
+#define BACKWARD 'B'
+
+char blueToothVal; //value sent over via BT
+
 enum state {Forward, Reverse, Left, Right, Stationary};
 state moveState = Stationary;
 
 void TaskRedLED(void *p);
-void TaskGreen(void *p);
+void TaskGreenLED(void *p);
 void TaskSerial(void *p);
 void TaskMotor(void *p);
 void TaskAudio(void *p);
